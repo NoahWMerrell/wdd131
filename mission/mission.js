5173f048cbcacbefe6a27aea3e0a54e539332fbf
NoahWMerrell <168214327+NoahWMerrell@users.noreply.github.com>
2025-04-30 13:03:20 -0600

    Adjusted syntax

--- mission/mission.js
+++ mission/mission.js
@@ -1,11 +1,10 @@
-let selectElem = document.querySelector('select');
-let logo = document.querySelector('img');
+let selectElem = document.querySelector('select'); // Add variable for select
+let logo = document.querySelector('#logo'); // Add variable for logo
 
-selectElem.addEventListener('change', changeTheme);
+selectElem.addEventListener('change', changeTheme); // When select changes runs the changeTheme function
 
 function changeTheme() {
     let current = selectElem.value; // Get value from selector
-    const logo = document.querySelector('#logo'); // Add variable for logo
     if (current == 'dark') {
         document.body.className = 'dark'; // Change body to dark by adding the dark class
         logo.setAttribute('src', 'byui-logo_white.png'); // Change logo to the new logo
