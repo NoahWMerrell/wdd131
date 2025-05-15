2b2eedc2aa39553d015722f92376de68904bc75f
NoahWMerrell <168214327+NoahWMerrell@users.noreply.github.com>
2025-05-15 12:20:24 -0600

    Finished up part 2

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,3 +1,4 @@
+// Menu button to toggle menu
 const menuButton = document.querySelector("#menu-button");
 function toggleMenu() {
   const menu = document.querySelector("#menu"); 
@@ -6,6 +7,7 @@ function toggleMenu() {
 
 menuButton.addEventListener("click", toggleMenu);
 
+// Reveal menu when screen is greater than 1000px
 function handleResize() {
   const menu = document.querySelector("#menu");
   if (window.innerWidth > 1000) {
@@ -17,3 +19,26 @@ function handleResize() {
 
 handleResize();
 window.addEventListener("resize", handleResize);
+
+// Add click event for images
+const gallery = document.querySelector(".gallery");
+const modal = document.querySelector(".modal");
+const closeViewer = document.querySelector(".close-viewer");
+
+gallery.querySelectorAll('img').forEach(image => {
+  image.addEventListener('click', () => {
+    modal.querySelector('img').src = image.src;
+    modal.querySelector('img').alt = image.alt;
+    modal.showModal();
+  });
+});
+
+// Close box when 'X' is clicked
+closeViewer.addEventListener('click', () => modal.close());
+
+// Close when user clicks outside of image
+modal.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.close();
+  }
+})
