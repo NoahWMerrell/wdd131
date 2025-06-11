bbd08985ae0285ec42ea762f7958d01f11280ab5
NoahWMerrell <168214327+NoahWMerrell@users.noreply.github.com>
2025-06-11 14:29:33 -0600

    Improved formatting and lighthouse score

--- character-card/script.js
+++ character-card/script.js
@@ -3,7 +3,7 @@ const character = {
       class: "Swamp Beat Diplomat",
       level: 5,
       health: 100,
-      image: 'snortleblat.jpg',
+      image: 'snortleblat.webp',
       attacked() {
         if (this.health >= 20) {
           // this.level -= 1;
@@ -18,24 +18,30 @@ const character = {
       }
     };
 
-document.querySelector('.image').src = character.image;
-document.querySelector('.name').textContent = character.name;
-document.querySelector('#class').textContent = character.class;
+// Define querySelectors
+let character_image = document.querySelector('.image');
+let character_name = document.querySelector('.name');
+let character_class = document.querySelector('#class');
+let character_level = document.querySelector('#level');
+let character_health = document.querySelector('#health');
 
-let level = document.querySelector('#level');
-level.textContent = character.level;
+// Set selected queries as their appropriate value
+character_image.src = character.image;
+character_name.textContent = character.name;
+character_class.textContent = character.class;
+character_level.textContent = character.level;
+character_health.textContent = character.health;
 
-let health = document.querySelector('#health');
-health.textContent = character.health;
-
+// When the "Attacked" button is clicked
 document.querySelector('#attacked').addEventListener('click', function() {
     character.attacked();
-    level.textContent = character.level;
-    health.textContent = character.health;
+    character_level.textContent = character.level;
+    character_health.textContent = character.health;
 });
 
+// When the "Level Up" button is clicked
 document.querySelector('#levelup').addEventListener('click', function() {
     character.levelUp();
-    level.textContent = character.level;
-    health.textContent = character.health;
+    character_level.textContent = character.level;
+    character_health.textContent = character.health;
 });
